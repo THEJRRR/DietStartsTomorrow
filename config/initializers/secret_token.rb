a6e8d8f196991062b8c7ea1b2334a9835bafbc39
John Wade <JohnRobertWade@gmail.com>
2014-04-07 18:23:38 -0700

    Initial rails framework created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DietStartsTomorrow::Application.config.secret_key_base = '6582b3a9d8d065244608e8e6de44470bcd2362161e30c6240970ccdc26d27c1db31d100128cb9ec8717ce347f23c1d3d7ca06ef03c51f388d796f0691ad4363b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DietStartsTomorrow::Application.config.session_store :cookie_store, key: '_diet_starts_tomorrow_session'
